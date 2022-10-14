a803baa2f89dd527e2a9fc85fb045795fe495cd6
NADaza122 <an.nidaza.122n@gmail.com>
2022-10-14 07:43:57 -0500

    clase14oct

--- async/2_callback_arrow.js
+++ async/2_callback_arrow.js
@@ -3,13 +3,14 @@ let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 //1.Generamos el ENDPOINT
 let endpoint = 'https://pokeapi.co/api/v2/type'
 
+//MODO ASINCRONO
 //1.0 Creamos la funcion de exito de la API
 const exito= (response) =>{
     const tipos = JSON.parse(response).results
     tipos.forEach((tipo)=>{
         console.log('------------------------')
+        console.log(' ')
         console.log(`Tipo: ${tipo.name}`)
-        console.log('------------------------')
     })
     //console.log(JSON.parse(response))
 }
@@ -21,7 +22,7 @@ const fallo=(status) =>{
 
 //1.2 Creamos la funcion que trae todo de la API
 const get_data = (endpoint, exito, fallo) => {
-    //2.Creamos el objeto de conexion de la API
+    //2.Creamos el objeto de conexion de la API(XMLhttp)
     let http = new XMLHttpRequest()
     //3.Abrir conexion a la API
     http.open('get', endpoint)
